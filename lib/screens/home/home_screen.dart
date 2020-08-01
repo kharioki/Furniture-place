b2ef9aba5096cdfbe5ef8f7c476cd94c4f9a8a2d
kharioki <antolisah@gmail.com>
2020-08-01 09:00:58 +0300

    home screen appbar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:furniture_app/constants.dart';
+import 'package:furniture_app/size_config.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // it helps to make our UI responsive
+    SizeConfig().init(context);
+
     return Scaffold(
-      appBar: AppBar(),
+      appBar: buildAppBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      leading: IconButton(
+        icon: SvgPicture.asset(
+          'assets/icons/menu.svg',
+          height: SizeConfig.defaultSize * 2, // 20
+        ),
+        onPressed: () {},
+      ),
+      actions: [
+        IconButton(
+          icon: SvgPicture.asset(
+            'assets/icons/scan.svg',
+            height: SizeConfig.defaultSize * 2.4, // 24
+          ),
+          onPressed: () {},
+        ),
+        Center(
+          child: Text(
+            'Scan',
+            style: TextStyle(
+              color: kTextColor,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        SizedBox(width: SizeConfig.defaultSize),
+      ],
     );
   }
 }
