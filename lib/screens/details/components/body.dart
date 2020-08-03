f339dcfe78f11645ed18e1085d3b5c1914afe4a4
kharioki <antolisah@gmail.com>
2020-08-03 22:47:49 +0300

    product description

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:furniture_app/constants.dart';
 import 'package:furniture_app/models/Product.dart';
 import 'package:furniture_app/screens/details/components/product_info.dart';
 import 'package:furniture_app/size_config.dart';
@@ -16,13 +17,79 @@ class Body extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ProductInfo(product: product),
-          Container(
-            height: 500,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(defaultSize * 1.2),
-                topRight: Radius.circular(defaultSize * 1.2),
+          ProductDescription(
+            product: product,
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProductDescription extends StatelessWidget {
+  const ProductDescription({
+    Key key,
+    @required this.product,
+    this.press,
+  }) : super(key: key);
+
+  final Product product;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    double defaultSize = SizeConfig.defaultSize;
+    return Container(
+      constraints: BoxConstraints(minHeight: defaultSize * 44),
+      padding: EdgeInsets.only(
+        top: defaultSize * 9, // 90
+        left: defaultSize * 2, // 20
+        right: defaultSize * 2, // 20
+      ),
+      // height: 500,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(defaultSize * 1.2),
+          topRight: Radius.circular(defaultSize * 1.2),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            product.subTitle,
+            style: TextStyle(
+              fontSize: defaultSize * 1.8,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: defaultSize * 3),
+          Text(
+            product.description,
+            style: TextStyle(
+              color: kTextColor.withOpacity(0.7),
+              height: 1.5,
+            ),
+          ),
+          SizedBox(height: defaultSize * 3),
+          SizedBox(
+            width: double.infinity,
+            child: FlatButton(
+              padding: EdgeInsets.all(defaultSize * 1.5),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50),
+              ),
+              color: kPrimaryColor,
+              onPressed: press,
+              child: Text(
+                'Add to Cart',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: defaultSize * 1.6, // 16
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
